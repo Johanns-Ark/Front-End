1d3db39e30fa86730724e10285ab392b138cec10
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2020-01-03 12:21:06 -0500

    Kim is awesome == Skyler

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import AddVideo from './components/pages/AddEditVids/AddVideo'
-import Editvideo from './components/pages/AddEditVids/Editvideo'
+import Editvideo from './components/pages/AddEditVids/EditVideo'
 // import NavBar from './components/pages/navbar/NavBar';
 import AppRouter from './components/configs/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      <AddVideo />
-      <Editvideo /> 
-      {/* <NavBar /> */}
-{/* <AppRouter /> */}
+      
+{ /*   <NavBar /> */ }
+ <AppRouter /> 
     </div>
   );
 }
--- src/components/configs/AppRouter.jsx
+++ src/components/configs/AppRouter.jsx
@@ -4,6 +4,9 @@ import Home from '../pages/main/Home'
 import Login from '../pages/configs/Login'
 import PrivateRoute from './PrivateRoute'
 import LoggedIn from '../pages/main/LoggedIn'
+import AddVideo from '../pages/AddEditVids/AddVideo'
+import EditVideo from '../pages/AddEditVids/EditVideo'
+
 
 const AppRouter = () => {
     return (
@@ -11,6 +14,8 @@ const AppRouter = () => {
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route  path='/login' component={Login} />
+            <PrivateRoute  path='/addvideo' component={AddVideo} />
+            <PrivateRoute  path='/editvideo' component={EditVideo} />
             <PrivateRoute path='/login/login' component={LoggedIn} />
           </Switch>  
         </>
